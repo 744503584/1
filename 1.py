5065c72bb9044088f49324dd502c4d0e17c6a057
744503584 <48125392+744503584@users.noreply.github.com>
2019-03-11 08:36:17 +0800

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,4 @@
+total=1
+for i in range(101):
+    total=total+i
+print("1-100的和为:",total)
---
+++ 2.py
@@ -0,0 +1,6 @@
+print("1-100的奇数为:")
+for i in range(101):
+    if i%2!=0:
+        print(i,end=',')
+    else:
+        continue;
